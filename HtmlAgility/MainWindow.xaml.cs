ebbe4d5adbb5d28c539b2bb991bcec901c2cd0ae
verhofstadt <bernd@live.be>
2013-12-04 21:40:28 +0100

    Code Optimalisation

--- HtmlAgility/MainWindow.xaml.cs
+++ HtmlAgility/MainWindow.xaml.cs
@@ -1,21 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using HtmlAgilityPack;
-using System.Diagnostics;
 using System.Windows.Threading;
+using HtmlAgilityPack;
 
 /* Bernd Verhofstadt
  * Artesis Plantijn Hogeschool Antwerpen
@@ -23,25 +15,27 @@ using System.Windows.Threading;
  */
 
 
-namespace HtmlAgility
+namespace Tweakers
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
+    
 
-    /*TODO
-     * Delete id="menu"
-     * Delete class = "tnetUrl"
-     * Delete id="footer"
+    /* 
+     * TODO
+     * Delete from html: id="menu"
+     * Delete from html: class = "tnetUrl"
+     * Delete from html: id="footer"
      */
 
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-        private int l = 0;
-        private string[] linkPage = new string[100]; 
-        private string[] titlePage = new string[100];
-        DispatcherTimer ProgressWebsite;
+        private int _l;
+        private readonly string[] _linkPage = new string[100]; 
+        private readonly string[] _titlePage = new string[100];
+        readonly DispatcherTimer _progressWebsite;
+        private double _inProcess;
         
         
         public MainWindow()
@@ -51,95 +45,91 @@ namespace HtmlAgility
             wc.DownloadStringCompleted += wc_DownloadStringCompleted;
             wc.DownloadStringAsync(new Uri("http://tweakers.mobi/"));
             DateTime time = DateTime.Now;
-            string format = "D";
-            DatumToday.Content = time.ToString(format);
-            ProgressWebsite = new DispatcherTimer(DispatcherPriority.Render);
+            DatumToday.Content = time.ToString("D");
+            _progressWebsite = new DispatcherTimer(DispatcherPriority.Render);
             InitializeTimer();
+            _inProcess = 0;
         }
 
         void wc_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
         {
-            
-            if (e.Error == null)
+            if (e.Error != null) return;
+            var scrape = new HtmlDocument();
+            scrape.LoadHtml(e.Result);
+            try
             {
-                
-                var scrape = new HtmlAgilityPack.HtmlDocument();
-                scrape.LoadHtml(e.Result);
-                try
-                {
                     
-                    HtmlNode docnode = scrape.DocumentNode;
-                    HtmlNode[] divnodes = docnode.Descendants("div").ToArray();
-                    for (int i = 0; i < divnodes.Length; i++)
+                HtmlNode docnode = scrape.DocumentNode;
+                HtmlNode[] divnodes = docnode.Descendants("div").ToArray();
+                foreach (HtmlNode t in divnodes)
+                {
+                    if (t.Attributes["class"] != null)
                     {
-                        if (divnodes[i].Attributes["class"] != null)
+                        if (t.Attributes["class"].Value == "content")
                         {
-                            if (divnodes[i].Attributes["class"].Value == "content")
+                            HtmlNode[] anodes = t.Descendants("a").ToArray();
+                            foreach (var htmlNode in anodes)
                             {
-                                HtmlNode[] anodes = divnodes[i].Descendants("a").ToArray();
-                                foreach (var htmlNode in anodes)
-                                {
-                                    linkPage[l] = Convert.ToString(htmlNode.Attributes["href"].Value);
-                                    titlePage[l] = htmlNode.InnerText;
+                                _linkPage[_l] = Convert.ToString(htmlNode.Attributes["href"].Value);
+                                _titlePage[_l] = htmlNode.InnerText;
 
-                                    if (titlePage[l].Contains("Nieuwsberichten van") || titlePage[l].Contains("nieuwsberichten van"))
-                                    {
-                                        listviewresult.Items.Add(new Run(titlePage[l]));
-                                        //MessageBox.Show(titlePage[l]);
-                                    }
-                                    else
-                                    {
-                                        listviewresult.Items.Add(titlePage[l]);
-                                    }
-                                    
-                                    l++;
+                                if (_titlePage[_l].Contains("Nieuwsberichten van") || _titlePage[_l].Contains("nieuwsberichten van"))
+                                {
+                                    listviewresult.Items.Add(new Run(_titlePage[_l]));
                                 }
+                                else
+                                {
+                                    listviewresult.Items.Add(_titlePage[_l]);
+                                }
+                                    
+                                _l++;
                             }
                         }
                     }
+                }
 
-                    try
-                    {
-                        ProgressBar.Value = ProgressBar.Minimum;
-                        ProgressWebsite.Start();
-                        BrowserPage.Navigate(linkPage[0]);
-                    }
-                    catch (Exception)
-                    {
-                        MessageBox.Show("Can't load page...");
-                        throw;
-                    }                    
+                try
+                {
+                    ProgressBar1.Value = ProgressBar1.Minimum;
+                    _progressWebsite.Start();
+                    BrowserPage.Navigate(_linkPage[0]);
                 }
                 catch (Exception)
                 {
-                    MessageBox.Show("Controleer je netwerkverbindeng","Netwerkprobleem",MessageBoxButton.OK,MessageBoxImage.Warning);
-                }
+                    MessageBox.Show("Can't load page...");
+                    throw;
+                }                    
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Controleer je netwerkverbindeng","Netwerkprobleem",MessageBoxButton.OK,MessageBoxImage.Warning);
             }
         }
 
         private void InitializeTimer()
         {
-            ProgressWebsite.Tick +=ProgressWebsite_Tick;
-            ProgressWebsite.Interval = new TimeSpan(0, 0, 0, 0, 100);
-            ProgressWebsite.IsEnabled = true;
+            _progressWebsite.Tick +=ProgressWebsite_Tick;
+            _progressWebsite.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            _progressWebsite.IsEnabled = true;
         }
 
         private void ProgressWebsite_Tick(object sender, EventArgs e)
         {
-            ProgressBar.Value += 1;
-            if (ProgressBar.Value == ProgressBar.Maximum)
-                ProgressWebsite.Stop();
+            _inProcess += 5;
+            ProgressBar1.SetPercent(_inProcess);
+            //if (ProgressBar1.Value == ProgressBar1.Maximum)
+            if (_inProcess == ProgressBar1.Maximum)
+                _progressWebsite.Stop();
         }
 
         private void listviewresult_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
             {
-                ProgressBar.Value = ProgressBar.Minimum;
-                ProgressWebsite.Start();
+                PageStart();
                 int selectedIndex = listviewresult.SelectedIndex;
-                //BrowserPage.Navigate("http://verhofstadt.eu");
-                BrowserPage.Navigate(linkPage[selectedIndex]);
+                //BrowserPage.Navigate("http://verhofstadt-creations.eu");
+                BrowserPage.Navigate(_linkPage[selectedIndex]);
             }
             catch (Exception)
             {
@@ -157,21 +147,32 @@ namespace HtmlAgility
         {
             try
             {
-                ProgressBar.Value = ProgressBar.Minimum;
-                ProgressWebsite.Start();
+                PageStart();
                 BrowserPage.GoBack();
             }
             catch (Exception) 
             {
-                ProgressWebsite.Stop();
-                ProgressBar.Value = ProgressBar.Maximum;
+                PageSucces();
             }
         }
 
         private void BrowserPage_LoadCompleted(object sender, NavigationEventArgs e)
         {
-            ProgressWebsite.Stop();
-            ProgressBar.Value = ProgressBar.Maximum;
-        }      
+            PageSucces();
+            
+        }
+        private void PageSucces()
+        {
+            _progressWebsite.Stop();
+            ProgressBar1.SetPercent(100);
+            ProgressBar1.Value = ProgressBar1.Minimum;
+        }
+        private void PageStart()
+        {
+            _inProcess = 0;
+            ProgressBar1.SetPercent(0);
+            ProgressBar1.Value = ProgressBar1.Minimum;
+            _progressWebsite.Start();
+        }
     }
 }
---
+++ HtmlAgility/ProgressBarExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Tweakers
+{
+    public static class ProgressBarExtensions
+    {
+        private static TimeSpan duration = TimeSpan.FromMilliseconds(500);
+
+        public static void SetPercent(this ProgressBar progressBar, double percentage)
+        {
+            DoubleAnimation animation = new DoubleAnimation(percentage, duration);
+            progressBar.BeginAnimation(ProgressBar.ValueProperty, animation);
+            duration = TimeSpan.FromMilliseconds(700);
+        }
+    }
+}
